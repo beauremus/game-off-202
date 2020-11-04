0fe8ba169f58495b889637518de6aba467a6a6ff
Beau Harrison <beauharrison@gmail.com>
2020-11-03 22:21:56 -0600

    Moon cycling from left to right

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,10 @@
 import '../node_modules/phaser/dist/phaser.js';
 
+const gameSpace = {
+    width: 800,
+    height: 600
+}
+
 export default class Demo extends Phaser.Scene
 {
     constructor ()
@@ -9,38 +14,48 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.image('moon', 'assets/moon.png')
+        // this.load.image('logo', 'assets/phaser3-logo.png');
+        // this.load.image('libs', 'assets/libs.png');
+        // this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
+        // this.load.glsl('stars', 'assets/starfields.glsl.js');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
+        const moon = this.add.image(-200, gameSpace.height/2, 'moon')
 
         this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
+            targets: moon,
+            x: gameSpace.width+200,
+            duration: 4000,
+            ease: 'Expo.inOut',
             repeat: -1
         })
+
+        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+
+        // this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+
+        // this.add.image(400, 300, 'libs');
+
+        // const logo = this.add.image(400, 70, 'logo');
+
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 350,
+        //     duration: 1500,
+        //     ease: 'Sine.inOut',
+        //     yoyo: true,
+        //     repeat: -1
+        // })
     }
 }
 
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
+    width: gameSpace.width,
+    height: gameSpace.height,
     scene: Demo
 };
 
